f0bbb43a6256a986ce628af4a10796177b5decc4
Dan97O <dan97-oddo@live.it>
2023-05-25 17:49:12 +0200

    add edit markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,38 @@
                 @foreach ($trains as $train)
                     <div class="card my-3 bg-secondary text-white">
                         <div class="card-body shadow">
-                            <h4 class="card-title"><strong> {{ $train->departure_station }}</strong> <i
-                                    class="fa fa-arrow-right" aria-hidden="true"></i> <strong> {{ $train->arrival_station }}
-                                </strong></h4>
-                            <p class="card-text"><strong> {{ $train->departure_time }}</strong> <i class="fa fa-arrow-right"
-                                    aria-hidden="true"></i><strong> {{ $train->arrival_time }}</strong></p>
-                            <p class="card-text">{{ $train->company }}</p>
+                            <div class="card-title d-flex justify-content-between">
+                                <h4>
+                                    <strong> {{ $train->departure_station }}
+                                    </strong>
+                                    <i class="fa fa-arrow-right" aria-hidden="true">
+                                    </i>
+                                    <strong>
+                                        {{ $train->arrival_station }}
+                                    </strong>
+                                </h4>
+                                <div>
+                                    <strong>Train code: </strong>{{ $train->train_code }}
+                                </div>
+                            </div>
+                            <p class="card-text">
+                                <strong>
+                                    {{ $train->departure_time }}
+                                </strong>
+                                <i class="fa fa-arrow-right" aria-hidden="true">
+                                </i>
+                                <strong>
+                                    {{ $train->arrival_time }}
+                                </strong>
+                            </p>
+                            <p class="card-text d-flex justify-content-between">
+                                <strong>
+                                    {{ $train->company }}
+                                </strong>
+                                <strong>N° Carriages:
+                                    {{ $train->number_carriages }}
+                                </strong>
+                            </p>
 
                             @if ($train->cancelled)
                                 <span class="badge text-bg-danger">Cancelled</span>
