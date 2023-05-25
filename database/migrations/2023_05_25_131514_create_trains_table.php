4bd9de53709feab27d9c68375efabbdc1a4a7095
Dan97O <dan97-oddo@live.it>
2023-05-25 15:41:51 +0200

    add table trains

--- database/migrations/2023_05_25_131514_create_trains_table.php
+++ database/migrations/2023_05_25_131514_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival _time');
+            $table->tinyInteger('train_code')->unsigned();
+            $table->tinyInteger('number_carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
